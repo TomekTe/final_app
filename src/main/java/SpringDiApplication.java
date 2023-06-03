2f7e01e2fe81c8b369da29d6dae8787aed06337c
TomekTe <ttkonto.fb@gmail.com>
2023-06-03 14:43:38 +0200

    (1) 03.06.2023

---
+++ src/main/java/SpringDiApplication.java
@@ -0,0 +1,9 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringDiApplication {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("beans.xml");
+        context.close();
+    }
+}
